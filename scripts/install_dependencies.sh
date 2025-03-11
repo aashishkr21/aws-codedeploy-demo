24f1b193dbb5c76cec02e5a8957ba9e0aeee394e
aashishkr21 <aashish.kumar@cloudzenia.com>
2025-03-11 18:57:53 +0000

    made changes in dependencies script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+set -e
+
+# Ensure the app directory belongs to ec2-user
+sudo chown -R ec2-user:ec2-user /home/ec2-user/app
+sudo chmod -R 755 /home/ec2-user/app
+
+# Run npm install as ec2-user
 cd /home/ec2-user/app
-npm install
+sudo -u ec2-user npm install
