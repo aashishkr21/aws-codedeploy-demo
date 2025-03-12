f6455edd5b3ccc035928d13fc6fd9d4067d41c1e
aashishkr21 <aashish.kumar@cloudzenia.com>
2025-03-12 13:06:17 +0530

    testing21

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from AWS CodeDeploy. If you have reached this far it means pipeline is working fine');
+  res.send('Hello from AWS CodeDeploy. If you have reached this far it means pipeline is working fine 100');
 });
 
 app.listen(port, () => {
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,19 +6,7 @@ echo "Starting the application with PM2..."
 cd /home/ec2-user/app  # Navigate to the app directory
 
 # Stop and delete existing PM2 process (ignore errors if not running)
-pm2 stop app || true  
-pm2 delete app || true  
-
-# Start the new version using PM2
-pm2 start app.js --name app
-
-# Ensure PM2 restarts on reboot
-pm2 startup systemd -u ec2-user --hp /home/ec2-user | tee /tmp/pm2_startup.log
-pm2 save
-
-# Wait for PM2 to ensure app is running before exiting (prevents CodeDeploy errors)
-sleep 5  
-pm2 list  # Show PM2 processes for debugging
+pm2 reload all
 
 echo "App started successfully with PM2!"
 exit 0  # Ensure script exits cleanly
