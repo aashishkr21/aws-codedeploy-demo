2a80a3c3d1ae11a2364f4880237fa9439164de7e
aashishkr21 <aashish.kumar@cloudzenia.com>
2025-03-12 06:28:40 +0000

    testing3

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from AWS CodeDeploy. Hope the pipeline is working great!');
+  res.send('Hello from AWS CodeDeploy. If you have reached this far it means pipeline is working fine');
 });
 
 app.listen(port, () => {
