60738e4e27f57f98da52695b66dc12f249f5f647
aashishkr21 <aashish.kumar@cloudzenia.com>
2025-05-14 16:49:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('This side Loda Lassan Ani');
+  res.send('Codepipeline is working!');
 });
 
 app.listen(port, '0.0.0.0', () => {
